af5747e3368a43f7673a308262e928fa606a64ad
Joaoojohn <joaopedro.cs12@hotmail.com>
2024-06-18 23:45:30 -0300

    add Swagger

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,5 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(s => s.CustomSchemaIds(n => n.FullName));
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello World!");
 
