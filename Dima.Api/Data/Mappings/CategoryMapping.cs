a2ef79b0d53c7b6db6649184abd9ac0ed1f569b1
Joaoojohn <joaopedro.cs12@hotmail.com>
2024-06-19 00:31:10 -0300

    Mapping models

---
+++ Dima.Api/Data/Mappings/CategoryMapping.cs
@@ -0,0 +1,23 @@
+﻿using Dima.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dima.Api.Data.Mappings
+{
+    public class CategoryMapping : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category");
+
+
+            builder.HasKey(i => i.Id);
+
+            builder.Property(t => t.Title).IsRequired().HasColumnType("NVARCHAR").HasMaxLength(80);
+            
+            builder.Property(d=> d.Description).IsRequired(false).HasColumnType("NVARCHAR").HasMaxLength(300);
+            
+            builder.Property(u => u.UserId).IsRequired().HasColumnType("VARCHAR").HasMaxLength(200);
+        }
+    }
+}
---
+++ Dima.Api/Data/Mappings/TransactionMapping.cs
@@ -0,0 +1,29 @@
+﻿using Dima.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dima.Api.Data.Mappings
+{
+    public class TransactionMapping : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.ToTable("Transaction");
+
+
+            builder.HasKey(i => i.Id);
+
+            builder.Property(t => t.Title).IsRequired().HasColumnType("NVARCHAR").HasMaxLength(80);
+
+            builder.Property(t => t.Type).IsRequired().HasColumnType("SMALLINT");
+
+            builder.Property(a => a.Amount).IsRequired().HasColumnType("MONEY");
+            
+            builder.Property(c => c.CreatedAt).IsRequired();
+
+            builder.Property(p => p.PaidOrReceivedAt).IsRequired(false);
+
+            builder.Property(u => u.UserId).IsRequired().HasColumnType("VARCHAR").HasMaxLength(200);
+        }
+    }
+}
