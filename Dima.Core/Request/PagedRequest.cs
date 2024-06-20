316d0544f54ddf4f9bb70e0452411385d784eea8
Joaoojohn <joaopedro.cs12@hotmail.com>
2024-06-19 21:56:29 -0300

    Base request, and base response

---
+++ Dima.Core/Request/PagedRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Request
+{
+    public abstract class PagedRequest : Request
+    {
+        public int PageNumber { get; set; } = Configuration.DefaultPageNumber;
+        public int PagaSize { get; set; } = Configuration.DefaultPageSize;
+    }
+}
---
+++ Dima.Core/Request/Request.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Request
+{
+    public abstract class Request
+    {
+        public string UserId { get; set; } = string.Empty;
+
+    }
+}
