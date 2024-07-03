6f98588bf10bf672984fc68e7f5bb42db0644ce2
Joaoojohn <joaopedro.cs12@hotmail.com>
2024-07-03 20:38:26 -0300

    Identity

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,28 +1,37 @@
 using Dima.Api.Data;
 using Dima.Api.Endpoints;
 using Dima.Api.Handers;
+using Dima.Api.Models;
 using Dima.Core.Handlers;
-using Dima.Core.Models;
-using Dima.Core.Requests.Categories;
-using Dima.Core.Responses;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>( d => d.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty));
-builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
-builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(s => s.CustomSchemaIds(n => n.FullName));
 
+builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme).AddIdentityCookies();
+builder.Services.AddAuthorization();
+
+builder.Services.AddDbContext<AppDbContext>( d => d.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty));
+builder.Services.AddIdentityCore<User>().AddRoles<IdentityRole<long>>().AddEntityFrameworkStores<AppDbContext>().AddApiEndpoints();
+
+builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
+
 app.MapGet("/", () => new { message = "is running" });
 app.MapEndpoints();
-
+app.MapGroup("v1/identity").WithTags("Identity").MapIdentityApi<User>();
 
 app.Run();
