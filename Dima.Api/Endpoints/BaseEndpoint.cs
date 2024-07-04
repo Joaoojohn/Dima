c8b768cc370f24cc086d69d3c32faa8b6da88d11
Joaoojohn <joaopedro.cs12@hotmail.com>
2024-07-03 21:40:39 -0300

    logout and getRoles endpoint

--- Dima.Api/Endpoints/BaseEndpoint.cs
+++ Dima.Api/Endpoints/BaseEndpoint.cs
@@ -1,6 +1,8 @@
 ﻿using Dima.Api.Common.Api;
 using Dima.Api.Endpoints.Categories;
+using Dima.Api.Endpoints.Identity;
 using Dima.Api.Endpoints.Transactions;
+using Dima.Api.Models;
 
 namespace Dima.Api.Endpoints
 {
@@ -9,6 +11,19 @@ namespace Dima.Api.Endpoints
         public static void MapEndpoints(this WebApplication app)
         {
             var endpoints = app.MapGroup("");
+
+            endpoints.MapGroup("/")
+            .WithTags("Health Check")
+            .MapGet("/", () => new { message = "is running" });
+
+            endpoints.MapGroup("v1/identity")
+            .WithTags("Identity")
+            .MapIdentityApi<User>();
+
+            endpoints.MapGroup("v1/identity")
+            .WithTags("Identity")
+            .MapEndpoint<LogoutEndpoint>()
+            .MapEndpoint<GetRolesEndpoint>();
 
             endpoints.MapGroup("v1/categories").WithTags("Categories")
             .RequireAuthorization()
